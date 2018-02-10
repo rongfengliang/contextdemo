b09eb00d55448b87098a94fe35c517c0966dee40
dalongrong <1141591465@qq.com>
2018-02-10 23:37:39 +0800

    add timer demo

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"sync"
 	"time"
 )
@@ -23,22 +22,33 @@ var (
 
 func main() {
 
-	start := time.Now()
-	wg.Add(MAXSIZE)
-	ctx := context.Background()
-	ctx2, cancel := context.WithTimeout(ctx, 200*time.Second)
-	//ctx2, cancel := context.WithCancel(ctx)
-	defer cancel()
-	for i := 0; i < MAXSIZE; i++ {
-		go func() {
-			v := login(ctx2, user{name: "dalong", appid: "dd"})
-			log.Println(v)
-		}()
-	}
-	wg.Wait()
-	log.Println(time.Since(start))
+	// start := time.Now()
+	// wg.Add(MAXSIZE)
+	// ctx := context.Background()
+	// ctx2, cancel := context.WithTimeout(ctx, 200*time.Second)
+	// //ctx2, cancel := context.WithCancel(ctx)
+	// defer cancel()
+	// for i := 0; i < MAXSIZE; i++ {
+	// 	go func() {
+	// 		v := login(ctx2, user{name: "dalong", appid: "dd"})
+	// 		log.Println(v)
+	// 	}()
+	// }
+	// wg.Wait()
+	// log.Println(time.Since(start))
+	timefuncdemo()
 }
+func timefuncdemo() {
+	t1 := time.NewTimer(time.Second * 5)
+	for {
+		select {
+		case <-t1.C:
+			println("5s timer")
+			t1.Reset(time.Second * 5)
+		}
 
+	}
+}
 func login(ctx context.Context, v interface{}) (result interface{}) {
 	//TODO
 	defer wg.Done()
